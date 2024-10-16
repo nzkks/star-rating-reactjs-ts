93a0a1cf6d04fafdece1d71eb776e830485c286c
nzkks <kkshanthosh@gmail.com>
2024-10-16 20:38:43 +1300

    Star rating: set input and output as like any other form element

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import './App.css';
 import { StarRating } from '@/components';
 
 const App = () => {
+  const handleOnChange = (value: number) => {
+    console.log(value);
+  };
+
   return (
     <>
       <h3>Star Rating using Reactjs and Typescript</h3>
 
-      <StarRating />
+      <StarRating value={2} onChange={handleOnChange} />
     </>
   );
 };
